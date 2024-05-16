446af5e2d2171719ec8e2ac8c7a328237b6a4557
Kacper Piasecki <k.r.piasecki2@gmail.com>
2024-05-16 14:38:02 +0200

    Adding Files

---
+++ src/main/java/pl/piasecki/financedesktopapp/FinanceDesktopAppApplication.java
@@ -0,0 +1,13 @@
+package pl.piasecki.financedesktopapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceDesktopAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinanceDesktopAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/piasecki/financedesktopapp/FinanceDesktopAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.piasecki.financedesktopapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceDesktopAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
